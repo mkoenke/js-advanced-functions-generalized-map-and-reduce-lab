173b069b469084d8949d78fa479bd5db41c45658
mkoenke <mkoenke@sas.upenn.edu>
2020-11-24 12:49:10 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let variable = array[i]
+      newArray.push(callback(variable))
+    }
+  
+    return newArray;
+  }
+  
+  function reduce(array, callback, starting){
+    let newArray = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      newArray = callback(array[i], newArray)
+    }
+  
+    return newArray;
+  }
